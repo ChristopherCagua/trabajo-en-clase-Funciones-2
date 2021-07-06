81dbd2ddc9ba27bf68e21ee3b78fc30448a1f938
ChristopherCagua <caguachristopher@gmail.com>
2021-07-05 19:17:23 -0500

    Trabajo en clase sobre funciones

---
+++ cmake-build-debug/CMakeFiles/Trabajo_funciones.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Trabajo_funciones.dir/main.cpp.obj"
+  "Trabajo_funciones.exe"
+  "Trabajo_funciones.exe.manifest"
+  "Trabajo_funciones.pdb"
+  "libTrabajo_funciones.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Trabajo_funciones.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
